0f2799fcd7b74400a9ee12b4ec476556e8ebad20
임근석 <rmstjr1030@nate.com>
2020-01-02 12:18:35 +0000

    UpdateCode

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # AutoBench-Electron
+
+## [MainBoard Database](https://motherboarddb.com/motherboards)
+### Crawling Point
+- General Information
+ - Manufactrurer
+ - Socket(s)
+ - Form Factor
+ - Chipset
+- Back IO
+ - USB
+- Internal Connections
+ - Storage
+ - USB
+- Memory
+ - This board has [n] slots with the following specifications:
+ - Slot Protocol
+ - Maximum Capacity
+- Expansion Slots
+ - Slots
+- M.2 Slots
--- crawling.js
+++ crawling.js
@@ -1,149 +1,91 @@
 const axios = require("axios");
-const request = require("request");
 const cheerio = require("cheerio");
+const fs = require('fs');
 
-
-const getHtml = async (url) => {
+const getHtml = async (url, i) => {
   try {
     return await axios.get(url);
   } catch (error) {
-    console.error(error);
+		return "error";
   }
 };
 
-request.get({url: 'http://autobench-web.run.goorm.io/mainboard_db.html'}, (err, res, body) => {
-	let ulList = [];
-  const $ = cheerio.load(body);
-	const $bodyList = $("div.col#table-wrapper").children("div.row").children("div.col");
-	console.log($bodyList)
-	$bodyList.each(function(i, elem) {
-		let detail = $(this).find("li").text().split("\t").join("").split("\n\n").join("\n").split(":").join("\n").split("\n");
-		
-		detail = detail.filter(function (el) {
-			if (el.length != 0)
-				return el;
+function cpudataget() {
+	getHtml("https://www.cpubenchmark.net/high_end_cpus.html").then(html => {
+		let ulList = [];
+		const $ = cheerio.load(html.data);
+		const $bodyList = $("div#mark ul.chartlist").children("li");
+
+		$bodyList.each(function(i, elem) {
+			ulList[i] = {
+					rank: i+1,
+					name: $(this).find('span.prdname').text(),
+					score: Number($(this).find('span.count').text().replace(',',''))
+			};
 		});
-		
-		if (detail.length == 16) {
-			ulList[i] = {
-				id: i+1,
-				name: $(this).find('h4').text(),
-				socket: detail[1],
-				form: detail[3].trim(),
-				chipset: detail[5].trim(),
-				support_ram: detail[7],
-				usb2: detail[13],
-				usb3: null,
-				sata: null
-			};
-		} else if (detail.length == 17){
-			ulList[i] = {
-				id: i+1,
-				name: $(this).find('h4').text(),
-				socket: detail[1],
-				form: detail[3].trim(),
-				chipset: detail[5].trim(),
-				support_ram: detail[7],
-				usb2: detail[13],
-				usb3: null,
-				sata: detail[16]
-			};
-		} else if (detail.length == 18) {
-			ulList[i] = {
-				id: i+1,
-				name: $(this).find('h4').text(),
-				socket: detail[1],
-				form: detail[3].trim(),
-				chipset: detail[5].trim(),
-				support_ram: detail[7],
-				usb2: detail[13],
-				usb3: detail[15],
-				sata: detail[17]
-			};
-		}
-	});
-	const data = ulList.filter(n => n.name);
-	
-	data.forEach(element => {
-    console.log(element);
-  });
-});
 
-request.get({url: 'https://motherboarddb.com/motherboards/?page=3&so=y'}, (err, res, body) => {
-	let ulList = [];
-  const $ = cheerio.load(body);
-	const $bodyList = $("div.col#table-wrapper")//.children("div.row").children("div.col");
-	console.log(body)
-	$bodyList.each(function(i, elem) {
-		let detail = $(this).find("li").text().split("\t").join("").split("\n\n").join("\n").split(":").join("\n").split("\n");
-		
-		detail = detail.filter(function (el) {
-			if (el.length != 0)
-				return el;
+		const data = ulList.filter(n => n.name);
+		return data;
+	}).then((res) => {
+		res.forEach(element => {
+			console.log(element);
 		});
-		
-		if (detail.length == 16) {
-			ulList[i] = {
-				id: i+1,
-				name: $(this).find('h4').text(),
-				socket: detail[1],
-				form: detail[3].trim(),
-				chipset: detail[5].trim(),
-				support_ram: detail[7],
-				usb2: detail[13],
-				usb3: null,
-				sata: null
-			};
-		} else if (detail.length == 17){
-			ulList[i] = {
-				id: i+1,
-				name: $(this).find('h4').text(),
-				socket: detail[1],
-				form: detail[3].trim(),
-				chipset: detail[5].trim(),
-				support_ram: detail[7],
-				usb2: detail[13],
-				usb3: null,
-				sata: detail[16]
-			};
-		} else if (detail.length == 18) {
-			ulList[i] = {
-				id: i+1,
-				name: $(this).find('h4').text(),
-				socket: detail[1],
-				form: detail[3].trim(),
-				chipset: detail[5].trim(),
-				support_ram: detail[7],
-				usb2: detail[13],
-				usb3: detail[15],
-				sata: detail[17]
-			};
-		}
 	});
-	const data = ulList.filter(n => n.name);
-	
-	data.forEach(element => {
-    console.log(element);
+}
+
+function getData(i) {
+  return new Promise(function (resolve, reject) {
+		const url = `https://motherboarddb.com/motherboards/${i}`;
+		let count = 0;
+		getHtml(url, i).then(html => {
+			if (html == "error"){
+				resolve(`${i}, null\n`);
+			}
+			let ulList = [];
+			const $ = cheerio.load(html.data);
+			const $bodyList = $("div.card-body");
+			const model_name = $("div.container").children("div.row").children("div.col").find("h1").text();
+			
+			$bodyList.each(function(j, elem) {
+				let detail = $(this).text().split("\n\n\n").join("\n").split("\n\n").join("\n").replace("\n","").split("\n");
+				detail = detail.filter(function (el) {
+					if (el.length != 0)
+						return el.trim();
+				});
+				count++;
+				// console.log(i, detail)
+				
+				/*
+				ulList[i] = {
+					id: i+1,
+					name: $(this).find('h4').text(),
+					socket: detail[1],
+					form: detail[3].trim(),
+					chipset: detail[5].trim(),
+					support_ram: detail[7],
+					usb2: detail[13],
+					usb3: null,
+					sata: null
+				};
+				*/
+			});
+			// const data = ulList.filter(n => n.name);
+
+			// data.forEach(element => {
+			// 	console.log(element);
+			// });
+		}).then(()=>{
+			resolve(`${i}, ${count}\n`);
+		});
   });
-});
+}
 
-// getHtml("https://www.cpubenchmark.net/high_end_cpus.html").then(html => {
-//   let ulList = [];
-//   const $ = cheerio.load(html.data);
-//   const $bodyList = $("div#mark ul.chartlist").children("li");
-
-//   $bodyList.each(function(i, elem) {
-//     ulList[i] = {
-//         rank: i+1,
-//         name: $(this).find('span.prdname').text(),
-//         score: Number($(this).find('span.count').text().replace(',',''))
-//     };
-//   });
-
-//   const data = ulList.filter(n => n.name);
-//   return data;
-// }).then((res) => {
-//   res.forEach(element => {
-//     console.log(element);
-//   });
-// });
+// 한번에 정상적으로 res 받을 수 있는 횟수는 50회
+// 9~14개의 플레그가 있다.
+for(var i = 1; i <= 50; i++){
+	getData(i).then(value => {
+		fs.appendFile('data1.csv', value, function (err) {
+			if (err) throw err;
+		});
+	});
+}
