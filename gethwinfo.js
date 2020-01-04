c1ceaa7e61949065384938713db54d99736af3f8
임근석 <rmstjr1030@nate.com>
2020-01-04 10:29:49 +0000

    UpdateCode

--- gethwinfo.js
+++ gethwinfo.js
@@ -1,54 +1,61 @@
-import wmic from 'ms-wmic'
+import wmic from 'ms-wmic';
 
-export function gethwinfo_win() {
-	let cpu = "hwinfo ---cpu";
-  let no_package = "lscpu | grep \"Model name\"";
-    
-  let gpu = "sudo lshw -C display";
-  let gpu2 = "lspci | grep -i --color 'vga\|3d\|2d'";
-    
-  let mem = "hwinfo --memory";
-  let mem_dmidecode = "dmidecode -t memory | grep \"Part Number\"";
-    
-  let mainboard = "dmidecode -s baseboard-product-name";
-  let mainboard2 = "dmidecode -t baseboard"
-    
-  let drive = "hwinfo -- disk";
+function gethwinfo_win_example() {
+	let cpu = 'hwinfo ---cpu';
+	let no_package = 'lscpu | grep "Model name"';
+
+	let gpu = 'sudo lshw -C display';
+	let gpu2 = "lspci | grep -i --color 'vga|3d|2d'";
+
+	let mem = 'hwinfo --memory';
+	let mem_dmidecode = 'dmidecode -t memory | grep "Part Number"';
+
+	let mainboard = 'dmidecode -s baseboard-product-name';
+	let mainboard2 = 'dmidecode -t baseboard';
+
+	let drive = 'hwinfo -- disk';
 }
 
-export function gethwinfo_win() {
-    return new Promise((resolve, reject) => {
-        wmic.execute('MEMORYCHIP get PartNumber', (err, stdOut) => {
-            if (err) reject(new Error("error case #mem"));
-            let mem = (stdOut);
-            for (var i = 0; i < 12; i++)
-                mem = mem.replace(/\r\r/,'\n');
-            mem = mem.split("\n");
-            
-            wmic.execute('CPU get Name', (err, stdOut) => {
-                if (err) reject(new Error("error case #cpu"));
-                let cpu = (stdOut).replace('(R)','').replace('(TM)','').replace('CPU ','');
-                
-                wmic.execute('BASEBOARD get Product', (err, stdOut) => {
-                    if (err) reject(new Error("error case #motherboard"));
-                    let board = (stdOut).replace('\r','');
+exports._linux = function() {
+	return new Promise((resolve, reject) => {
+		resolve({ MainBoard: 'board', CPU: 'cpu', GPU: 'gpu', RAM: 'mem', Drive: 'drive' });
+	});
+};
 
-                    wmic.execute('DISKDRIVE get Model', (err, stdOut) => {
-                        if (err) reject(new Error("error case #diskdrive"));
-                        let drive = (stdOut)
-                        for (var i = 0; i < 10; i++)
-                            drive = drive.replace(/\r\r/,'\n');
-                        drive = drive.split("\n");
+exports._win = function() {
+	return new Promise((resolve, reject) => {
+		wmic.execute('MEMORYCHIP get PartNumber', (err, stdOut) => {
+			if (err) reject(new Error('error case #mem'));
+			let mem = stdOut;
+			for (var i = 0; i < 12; i++) mem = mem.replace(/\r\r/, '\n');
+			mem = mem.split('\n');
 
-                        wmic.execute('path win32_VideoController get name', (err, stdOut) => {
-                            if (err) reject(new Error("error case #gpu"));
-                            let gpu = (stdOut).replace('NVIDIA ','').replace('AMD ', '');
+			wmic.execute('CPU get Name', (err, stdOut) => {
+				if (err) reject(new Error('error case #cpu'));
+				let cpu = stdOut
+					.replace('(R)', '')
+					.replace('(TM)', '')
+					.replace('CPU ', '');
 
-                            resolve({ 'MainBoard': board, 'CPU': cpu , 'GPU' : gpu, 'RAM' : mem, 'Drive' : drive});
-                        });
-                    });
-                });
-            });
-        });
-    });
-}
+				wmic.execute('BASEBOARD get Product', (err, stdOut) => {
+					if (err) reject(new Error('error case #motherboard'));
+					let board = stdOut.replace('\r', '');
+
+					wmic.execute('DISKDRIVE get Model', (err, stdOut) => {
+						if (err) reject(new Error('error case #diskdrive'));
+						let drive = stdOut;
+						for (var i = 0; i < 10; i++) drive = drive.replace(/\r\r/, '\n');
+						drive = drive.split('\n');
+
+						wmic.execute('path win32_VideoController get name', (err, stdOut) => {
+							if (err) reject(new Error('error case #gpu'));
+							let gpu = stdOut.replace('NVIDIA ', '').replace('AMD ', '');
+
+							resolve({ MainBoard: board, CPU: cpu, GPU: gpu, RAM: mem, Drive: drive });
+						});
+					});
+				});
+			});
+		});
+	});
+};
--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-import gethwinfo from './gethwinfo'
-import os from 'os'
+import gethwinfo from './gethwinfo';
+import os from 'os';
 
-if (os.platform() == "win32") {
-    gethwinfo().then((data) => {
-        let mb = Object.values(data)[0];
-        let cpu = Object.values(data)[1];
-        let gpu = Object.values(data)[2];
-        let ram = Object.values(data)[3];
-        let drive = Object.values(data)[4];
-        
-        console.log(mb)
-        console.log(cpu)
-        console.log(gpu)
-        console.log(ram) // 슬롯 여러개 존재 가능
-        console.log(drive) // 슬롯 여러개 존재 가능
-        
-        
-    }).catch(function (err) {
-        console.error(err);
-    });
+if (os.platform() == 'win32') {
+	gethwinfo
+		._win()
+		.then(data => {
+			let mb = Object.values(data)[0];
+			let cpu = Object.values(data)[1];
+			let gpu = Object.values(data)[2];
+			let ram = Object.values(data)[3];
+			let drive = Object.values(data)[4];
+
+			console.log(mb);
+			console.log(cpu);
+			console.log(gpu);
+			console.log(ram); // 슬롯 여러개 존재 가능
+			console.log(drive); // 슬롯 여러개 존재 가능
+		})
+		.catch(function(err) {
+			console.error(err);
+		});
+} else {
+	gethwinfo._linux().then(data => {
+		console.log(data);
+	});
 }
